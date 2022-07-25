95e8d5216e22ea81ea5ac928836de0cc3533135c
Ghislain LE MEUR <gigi206+github@gmail.com>
2022-07-25 23:41:31 +0200

    Fix wrong url for argo-worflows
    x

--- argocd/argo-workflows/argo-install.sh
+++ argocd/argo-workflows/argo-install.sh
@@ -4,5 +4,5 @@ if [ ! -x /usr/local/bin/argo ]
 then
     LAST_RELEASE=$(curl --silent https://api.github.com/repos/argoproj/argo-workflows/releases/latest | jq -r '.tag_name')
     echo "Downloading argo ${LAST_RELEASE}"
-    curl -L https://github.com/argoproj/argo-work^Cows/releases/download/v3.2.8/argo-linux-amd64.gz | gunzip -c > /usr/local/bin/argo && chmod +x /usr/local/bin/argo
+    curl -L https://github.com/argoproj/argo-workflows/releases/download/${LAST_RELEASE}/argo-linux-amd64.gz | gunzip -c > /usr/local/bin/argo && chmod +x /usr/local/bin/argo
 fi
