f6b2adf7416d30b7bd474350e76bed374bc5ce35
Ghislain LE MEUR <gigi206+github@gmail.com>
2022-08-11 14:30:35 +0200

    Add metrics to longhorn and create an argocd dependency for the csi-external-snapshotter

--- argocd/longhorn/install.sh
+++ argocd/longhorn/install.sh
@@ -3,8 +3,7 @@
 . "$(dirname $0)/../lib.sh"
 
 "$(dirname $0)/install-requirements.sh"
+require_app csi-external-snapshotter prometheus-stack
 install_app
 wait_app
 show_ressources
-
-# ${ARGOCD_CMD} app patch-resource ${APPNAME} --namespace ${NAMESPACE} --kind Service --resource-name longhorn-frontend --patch '{ "metadata": { "annotations": { "external-dns.alpha.kubernetes.io/hostname": "longhorn.gigix" } } }' --patch-type 'application/merge-patch+json'
