f5fddee323f51e9629ad2e3af35ac772128ef2f8
Ghislain LE MEUR <gigi206+github@gmail.com>
2022-08-15 20:28:09 +0200

    Bump linkerd-cni to 2.11.4

--- argocd/linkerd-cni/install.sh
+++ argocd/linkerd-cni/install.sh
@@ -2,10 +2,10 @@
 
 . "$(dirname $0)/../lib.sh"
 
+# Ensure the plugin is installed and ready
+linkerd check --pre --linkerd-cni-enabled --linkerd-namespace linkerd-cni
+
 require_app linkerd
 install_app
 wait_app
 show_ressources
-
-# ensure the plugin is installed and ready
-linkerd check --pre --linkerd-cni-enabled
