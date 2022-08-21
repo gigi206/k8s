0098dbaa555a41d36f2247a8afe379c3c802105c
Ghislain LE MEUR <gigi206+github@gmail.com>
2022-08-21 21:20:34 +0200

    Add prometheus-adapter

---
+++ argocd/prometheus-adapter/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+. "$(dirname $0)/../lib.sh"
+
+require_app prometheus-stack
+install_app
+wait_app
+show_ressources
---
+++ argocd/prometheus-adapter/uninstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+. "$(dirname $0)/../lib.sh"
+
+uninstall_app
