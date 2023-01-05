8e74d276a24c0a8dcf69c04ba35aead2290599e8
Ghislain LE MEUR <gigi206+github@gmail.com>
2023-01-05 16:04:30 +0100

    Fix typo

--- argocd/cert-manager/install.sh
+++ argocd/cert-manager/install.sh
@@ -2,11 +2,11 @@
 
 . "$(dirname $0)/../lib.sh"
 
-#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${VERSION}/cert-manager.crds.yaml
+# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${VERSION}/cert-manager.crds.yaml
 
 require_app prometheus-stack
 install_app
 wait_app
 # show_ressources
 $(dirname $0)/self-signed.sh
-Kubectl apply -f $(dirname $0)/prometheus.yaml
+kubectl apply -f $(dirname $0)/prometheus.yaml
