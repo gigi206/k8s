7210d8b155e1fc6097843cdabe5b36055a1dbdcc
Ghislain LE MEUR <gigi206+github@gmail.com>
2023-07-27 23:01:24 +0200

    Avoid cycling install
    longhorn calls prometheus-stack that calls longhorn (cycle)

--- argocd/longhorn/install.sh
+++ argocd/longhorn/install.sh
@@ -3,7 +3,8 @@
 . "$(dirname $0)/../lib.sh"
 
 "$(dirname $0)/install-requirements.sh"
-require_app csi-external-snapshotter prometheus-stack
+# require_app csi-external-snapshotter prometheus-stack
+require_app csi-external-snapshotter
 install_app
 wait_app
 show_ressources
