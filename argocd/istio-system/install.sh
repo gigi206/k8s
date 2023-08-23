bb63e1999d1c6495799fab1754b10aa7a1bf1a6c
Ghislain LE MEUR <gigi206+github@gmail.com>
2023-08-23 17:44:49 +0200

    Update istio-system

--- argocd/istio-system/install.sh
+++ argocd/istio-system/install.sh
@@ -2,7 +2,8 @@
 
 . "$(dirname $0)/../lib.sh"
 
-require_app gateway-api-controller
+# require_app gateway-api-controller opentelemetry loki-stack prometheus-stack jaeger
+require_app opentelemetry loki-stack prometheus-stack jaeger
 install_app
 wait_app
 # show_ressources
