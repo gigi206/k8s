a4684fe56299b393e86c93776c61e42f5483636b
Ghislain LE MEUR <gigi206+github@gmail.com>
2023-11-12 20:58:46 +0100

    Update Longhorn

--- argocd/longhorn/install-requirements.sh
+++ argocd/longhorn/install-requirements.sh
@@ -2,15 +2,3 @@
 
 apt install -y open-iscsi nfs-common util-linux curl bash grep
 systemctl enable --now iscsid.service
-
-cat <<EOF | kubectl apply -f -
-kind: VolumeSnapshotClass
-apiVersion: snapshot.storage.k8s.io/v1
-metadata:
-  name: longhorn
-driver: driver.longhorn.io
-# allowVolumeExpansion: true
-deletionPolicy: Delete
-EOF
-
-kubectl apply -f "$(dirname $0)/prometheus.yaml"
